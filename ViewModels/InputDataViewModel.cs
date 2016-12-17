29260b8754c610c895622300e612c41fc413e668
Splinter <splinter-cs@inbox.ru>
2016-12-17 15:02:32 +0300

    upd.

--- ViewModels/InputDataViewModel.cs
+++ ViewModels/InputDataViewModel.cs
@@ -6,68 +6,107 @@ using System.ComponentModel;
 
 namespace CreditCalc.ViewModels
 {
+	#region ViewModel
 	public class InputDataViewModel : Screen
 	{
+		#region Properties
+		[DisplayName("Сумма кредита")]
 		public string CreditSum { get; set; }
-		public string YearFee { get; set; }
-		public string CreditLength { get; set; }
-		public string MonthFee { get; set; }
-		public System.DateTime StartDate { get; set; }
-		private List<string> strings;
 
+		[DisplayName("Процентная ставка")]
+		public string YearFee { get; set; }
+
+		[DisplayName("Срок погашения")]
+		public string CreditLength { get; set; }
+
+		[DisplayName("Ежемесячная комиссия")]
+		public string MonthFee { get; set; }
+
+		public System.DateTime StartDate { get; set; }
+		#endregion
+
+		#region Variables
+		private List<string> properties;
+		/// <summary>
+		/// Mode 'One' for atleast one property, mode 'All' for all properties
+		/// </summary>
+		enum Mode { One, All };
+		#endregion
+
+		#region Methods
 		public InputDataViewModel(IModelValidator<InputDataViewModel> validator) : base(validator)
 		{
 			DisplayName = "ДАННЫЕ";
-			StartDate = System.DateTime.Today;			
+
+			// Setting current date to avoid empty date property
+			StartDate = System.DateTime.Today;
 		}
 
 		protected override void OnValidationStateChanged(IEnumerable<string> changedProperties)
-		{	
-			base.OnValidationStateChanged(changedProperties);
-			// Fody can't weave other assemblies, so we have to manually raise this
+		{
+			// Manual rasing notifier for Stylet properties
 			this.NotifyOfPropertyChange(() => this.CanCalculate);
 			this.NotifyOfPropertyChange(() => this.CanReset);
 		}
 
+		// Guard property for Calculate method
 		public bool CanCalculate
 		{
-			get { return !this.HasErrors && checkFields("all"); }
+			get { return !HasErrors && CheckFields(Mode.All); }
 		}
 
+		// Start calculation with inputed values
 		public void Calculate()
 		{
 
 		}
 
+		// Guard property for Reset method
 		public bool CanReset
 		{
-			get {return checkFields("one");}			
+			get { return CheckFields(Mode.One); }
 		}
 
+		// Reset all properties
 		public void Reset()
 		{
-			
+			CreditSum = string.Empty;
+			CreditLength = string.Empty;
+			YearFee = string.Empty;
+			MonthFee = string.Empty;
+			StartDate = System.DateTime.Today;
 		}
 
-		private bool checkFields(string mode)
+		/// <summary>
+		/// Check properties for Null or Empty values
+		/// </summary>
+		/// <param name="mode"></param>
+		private bool CheckFields(Mode mode)
 		{
-			strings = new List<string> { CreditSum, YearFee, CreditLength, MonthFee };
-			if (mode == "one")
-				return strings.Exists(x => !string.IsNullOrEmpty(x));
-			if (mode == "all")
-				return strings.All(x => !string.IsNullOrEmpty(x));
+			properties = new List<string> { CreditSum, CreditLength, YearFee, MonthFee };
+			if (mode == Mode.One)
+				return properties.Any(x => !string.IsNullOrEmpty(x));
+			if (mode == Mode.All)
+				return properties.All(x => !string.IsNullOrEmpty(x));
 			return false;
 		}
+		#endregion
 	}
+	#endregion
 
-	public class InputDataViewModelValidator : AbstractValidator<InputDataViewModel>
+	#region ValidationRules
+	// Entity validator for ViewModel
+	class InputDataViewModelValidator : AbstractValidator<InputDataViewModel>
 	{
 		public InputDataViewModelValidator()
 		{
-			RuleFor(x => x.CreditSum).NotEmpty().WithMessage("Поле не может быть пустым").Matches(@"^\d+$").WithMessage("Разрешены только числовые значения");
-			RuleFor(x => x.YearFee).NotEmpty().WithMessage("Поле не может быть пустым").Matches(@"^\d+$").WithMessage("Разрешены только числовые значения").Length(2).WithMessage("Разрешены только двухзначные значения");
-			RuleFor(x => x.CreditLength).NotEmpty().WithMessage("Поле не может быть пустым").Matches(@"^\d+$").Length(2).WithMessage("Сумма кредита must contain a number");
-			RuleFor(x => x.MonthFee).NotEmpty().Matches(@"^\d+$").WithMessage("{PropertyName} must be a valid number");			
+			CascadeMode = CascadeMode.StopOnFirstFailure;
+
+			RuleFor(x => x.CreditSum).NotEmpty().WithMessage("{PropertyName} не может быть пустым").Matches(@"^\d+$").WithMessage("Разрешены только числовые значения");
+			RuleFor(x => x.YearFee).NotEmpty().WithMessage("Поле не может быть пустым").Matches(@"^\d+(\.\d{1,2})?$").WithMessage("Разрешены только числовые значения");
+			RuleFor(x => x.CreditLength).NotEmpty().WithMessage("Поле не может быть пустым").Matches(@"^\d+$").WithMessage("Разрешены только числовые значения");
+			RuleFor(x => x.MonthFee).NotEmpty().WithMessage("Поле не может быть пустым").Matches(@"^\d+(\.\d{1,2})?$").WithMessage("Разрешены только числовые значения");
 		}
 	}
+	#endregion
 }
